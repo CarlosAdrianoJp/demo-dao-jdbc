167f45766dcf3e3142752b46d45937de9e936358
carlosadriano <calrpb@gmail.com>
2021-11-06 01:21:09 -0300

    aula-1

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,13 @@
+package aplicacao;
+
+import model.entities.Departamento;
+
+public class Programa {
+
+	public static void main(String[] args) {
+
+		Departamento obj = new Departamento(1, "livros");
+
+		System.out.println(obj);
+	}
+}
